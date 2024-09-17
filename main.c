7e9b75f20b79d7976bd4d5efedb7f3862ad9e523
EstebanBeltr4n <eescarraga@unicauca.edu.co>
2024-09-17 17:00:07 -0500

    Programa verificado y comentado, cumple con los requisitos

--- main.c
+++ main.c
@@ -2,18 +2,14 @@
 //Realizado por Johan Ordoñez y Esteban Escarraga
 //C. Ejercicio 3: Cálculo del área de un triángulo utilizando funciones
 
-// Funcion para calcular el area de un triangulo
-void calcular_area(float *base, float *altura, float *area) {
-    *area = (*base * *altura) / 2;
-}
+/*
+* Descripción: Escribe un programa que calcule el área de un triángulo dados la base y la altura.
+Debes utilizar una función para calcular el área y pasar los valores por referencia.
+ */
 
-// Funcion para solicitar los valores de base y altura al usuario
-void solicitar_valores(float *base, float *altura) {
-    printf("Introduce la base del triangulo: ");
-    scanf("%f", base);
-    printf("Introduce la altura del triangulo: ");
-    scanf("%f", altura);
-}
+//Llamado de funciones antes del main
+void calcular_area(float *base, float *altura, float *area);
+void solicitar_valores(float *base, float *altura);
 
 int main() {
     float base, altura, area;
@@ -25,7 +21,31 @@ int main() {
     calcular_area(&base, &altura, &area);
 
     // Mostrar el area calculada
-    printf("El area del triangulo es: %.2f\n", area);
+    printf("El area del triangulo es: %.2f metros cuadrados\n", area);
 
     return 0;
+}
+//Creacion de funciones
+
+/*Definir una función calcular_area(float *base, float *altura, float *area) que calcule el
+área usando los punteros para los valores de la base, la altura y el área resultante.
+*/
+// Funcion para calcular el area de un triangulo
+void calcular_area(float *base, float *altura, float *area) {
+    *area = (*base * *altura) / 2;
+}
+
+/*
+*Solicitar al usuario los valores de la base y altura, luego calcular y mostrar el área del
+triángulo.
+ *
+ */
+
+// Funcion para solicitar los valores de base y altura al usuario
+void solicitar_valores(float *base, float *altura) {
+    printf("******************* Programa para calcular el area del trinagulo *******************\n");
+    printf("Introduce la base (m) del triangulo:_ \n");
+    scanf("%f", base);
+    printf("Introduce la altura (m) del triangulo:_ \n");
+    scanf("%f", altura);
 }
